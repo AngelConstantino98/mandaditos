9e87395921d83768b5270d5d569a1ae002029483
Angel Constantino <angelconstantino98@gmail.com>
2026-07-03 16:51:16 -0600

    Agregar menu Tortas El Guero

--- src/negocios.js
+++ src/negocios.js
@@ -17,14 +17,28 @@ const GUISOS_EXTRA_TLAYUDA = [
 ];
 
 // Quesadillas, burritos y sopes:
-// 1 guiso = $60
-// 2 guisos = $65
-// 3 o más guisos = $75
+// quesillo no cuenta como carne
+// 1 carne = $60
+// 2 carnes = $65
+// 3 o más carnes = $75
 const PRECIOS_QUESADILLA_BURRITO_SOPE = {
   uno: 60,
   dos: 65,
   tresOMas: 75,
 };
+
+const EXTRAS_HAMBURGUESAS_EL_GUERO = [
+  {
+    id: "extra-carne-el-guero",
+    nombre: "Extra carne",
+    precio: 15,
+  },
+  {
+    id: "extra-quesillo-el-guero",
+    nombre: "Extra quesillo",
+    precio: 15,
+  },
+];
 
 const negocios = [
   {
@@ -39,7 +53,8 @@ const negocios = [
         id: "quesadillas-la-bendicion",
         nombre: "Quesadillas",
         precio: 60,
-        descripcion: "Quesillo opcional + 1 carne $60, 2 carnes $65, 3 o más carnes $75",
+        descripcion:
+          "Quesillo opcional + 1 carne $60, 2 carnes $65, 3 o más carnes $75",
         imagen: "/productos/quesadillas-la-bendicion.jpg",
         guisos: GUISOS_LA_BENDICION,
         preciosPorGuisos: PRECIOS_QUESADILLA_BURRITO_SOPE,
@@ -48,7 +63,8 @@ const negocios = [
         id: "burritos-la-bendicion",
         nombre: "Burritos",
         precio: 60,
-        descripcion: "Quesillo opcional + 1 carne $60, 2 carnes $65, 3 o más carnes $75",
+        descripcion:
+          "Quesillo opcional + 1 carne $60, 2 carnes $65, 3 o más carnes $75",
         imagen: "/productos/burritos-la-bendicion.jpg",
         guisos: GUISOS_LA_BENDICION,
         preciosPorGuisos: PRECIOS_QUESADILLA_BURRITO_SOPE,
@@ -57,7 +73,8 @@ const negocios = [
         id: "sopes-la-bendicion",
         nombre: "Sopes",
         precio: 60,
-        descripcion: "Quesillo opcional + 1 carne $60, 2 carnes $65, 3 o más carnes $75",
+        descripcion:
+          "Quesillo opcional + 1 carne $60, 2 carnes $65, 3 o más carnes $75",
         imagen: "/productos/sopes-la-bendicion.jpg",
         guisos: GUISOS_LA_BENDICION,
         preciosPorGuisos: PRECIOS_QUESADILLA_BURRITO_SOPE,
@@ -82,7 +99,8 @@ const negocios = [
         permitirSinGuisos: true,
         maxGuisos: 2,
         cantidadExactaGuisosExtra: 2,
-        textoSelector: "Incluye asada con quesillo. Puedes elegir 2 guisos extra:",
+        textoSelector:
+          "Incluye asada con quesillo. Puedes elegir 2 guisos extra:",
       },
       {
         id: "empanadas-la-bendicion",
@@ -97,6 +115,156 @@ const negocios = [
         precio: 50,
         descripcion: "Orden con 3 tostadas de asada",
         imagen: "/productos/tostadas-la-bendicion.jpg",
+      },
+    ],
+  },
+
+  {
+    id: "tortas-el-guero",
+    nombre: "Tortas El Güero",
+    emoji: "🥪",
+    descripcion: "Tortas, hot dogs y hamburguesas",
+    imagen: "/negocios/tortas-el-guero.jpg",
+    productos: [
+      // TORTAS
+      {
+        id: "torta-combinada-el-guero",
+        nombre: "Torta combinada",
+        precio: 55,
+        descripcion: "Carnes frías",
+        imagen: "/productos/tortas-el-guero.jpg",
+      },
+      {
+        id: "torta-pollo-el-guero",
+        nombre: "Torta de pollo",
+        precio: 55,
+        descripcion: "Torta de pollo",
+        imagen: "/productos/tortas-el-guero.jpg",
+      },
+      {
+        id: "torta-combinada-con-pollo-el-guero",
+        nombre: "Torta combinada c/pollo",
+        precio: 60,
+        descripcion: "Combinada con pollo",
+        imagen: "/productos/tortas-el-guero.jpg",
+      },
+      {
+        id: "torta-milanesa-el-guero",
+        nombre: "Torta de milanesa",
+        precio: 55,
+        descripcion: "Torta de milanesa",
+        imagen: "/productos/tortas-el-guero.jpg",
+      },
+      {
+        id: "torta-milanesa-con-pollo-el-guero",
+        nombre: "Torta milanesa con pollo",
+        precio: 60,
+        descripcion: "Milanesa con pollo",
+        imagen: "/productos/tortas-el-guero.jpg",
+      },
+      {
+        id: "torta-asada-el-guero",
+        nombre: "Torta de asada",
+        precio: 60,
+        descripcion: "Torta de asada",
+        imagen: "/productos/tortas-el-guero.jpg",
+      },
+      {
+        id: "torta-asada-con-pollo-el-guero",
+        nombre: "Torta asada c/pollo",
+        precio: 65,
+        descripcion: "Asada con pollo",
+        imagen: "/productos/tortas-el-guero.jpg",
+      },
+      {
+        id: "torta-campechana-el-guero",
+        nombre: "Torta campechana",
+        precio: 60,
+        descripcion: "Torta campechana",
+        imagen: "/productos/tortas-el-guero.jpg",
+      },
+      {
+        id: "torta-salchicha-con-huevo-el-guero",
+        nombre: "Torta salchicha con huevo",
+        precio: 50,
+        descripcion: "Salchicha con huevo",
+        imagen: "/productos/tortas-el-guero.jpg",
+      },
+
+      // HOT DOGS
+      {
+        id: "hotdog-tradicional-el-guero",
+        nombre: "Hot dog tradicional",
+        precio: 35,
+        descripcion: "Hot dog tradicional",
+        imagen: "/productos/hotdogs-el-guero.jpg",
+      },
+      {
+        id: "hotdog-milanesa-el-guero",
+        nombre: "Hot dog de milanesa",
+        precio: 40,
+        descripcion: "Hot dog de milanesa",
+        imagen: "/productos/hotdogs-el-guero.jpg",
+      },
+      {
+        id: "hotdog-pollo-el-guero",
+        nombre: "Hot dog de pollo",
+        precio: 40,
+        descripcion: "Hot dog de pollo",
+        imagen: "/productos/hotdogs-el-guero.jpg",
+      },
+      {
+        id: "hotdog-doble-el-guero",
+        nombre: "Hot dog doble",
+        precio: 40,
+        descripcion: "Hot dog doble",
+        imagen: "/productos/hotdogs-el-guero.jpg",
+      },
+      {
+        id: "hotdog-especial-el-guero",
+        nombre: "Hot dog especial",
+        precio: 40,
+        descripcion: "Hot dog especial",
+        imagen: "/productos/hotdogs-el-guero.jpg",
+      },
+      {
+        id: "hotdog-combinado-el-guero",
+        nombre: "Hot dog combinado",
+        precio: 40,
+        descripcion: "Hot dog combinado",
+        imagen: "/productos/hotdogs-el-guero.jpg",
+      },
+      {
+        id: "hotdog-pikachu-el-guero",
+        nombre: "Hot dog Pikachu",
+        precio: 40,
+        descripcion: "Hot dog Pikachu",
+        imagen: "/productos/hotdogs-el-guero.jpg",
+      },
+      {
+        id: "hotdog-salchicha-con-tocino-el-guero",
+        nombre: "Hot dog salchicha con tocino",
+        precio: 40,
+        descripcion: "Salchicha con tocino",
+        imagen: "/productos/hotdogs-el-guero.jpg",
+      },
+
+      // HAMBURGUESAS
+      {
+        id: "hamburguesa-tradicional-el-guero",
+        nombre: "Hamburguesa tradicional",
+        precio: 90,
+        descripcion: "Hamburguesa tradicional. Puedes agregar extras.",
+        imagen: "/productos/hamburguesas-el-guero.jpg",
+        extras: EXTRAS_HAMBURGUESAS_EL_GUERO,
+      },
+      {
+        id: "hamburguesa-especial-el-guero",
+        nombre: "Hamburguesa especial",
+        precio: 115,
+        descripcion: "Doble carne, piña, queso amarillo y quesillo. Puedes agregar extras.",
+        imagen: "/productos/hamburguesas-el-guero.jpg",
+        extras: EXTRAS_HAMBURGUESAS_EL_GUERO,
       },
     ],
   },
